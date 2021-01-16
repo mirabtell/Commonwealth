3b2119294956118070071ccffc846a3b9a9c9bef
foadolfat <49184464+foadolfat@users.noreply.github.com>
2021-01-16 10:25:14 -0800

    Add files via upload
    Initial javafx project

---
+++ commonwealth/src/sample/Controller.java
@@ -0,0 +1,20 @@
+package sample;
+
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    public Controller() {
+    }
+    @FXML
+    private void initialize() {
+    }
+    @FXML
+    public static void test(){
+        System.out.println("Hello");
+    }
+    @FXML
+    public static void running(){
+        System.out.println("Hello2");
+    }
+}
