0de0f88831ec3e3b00c20320d1f7b24d01a824ae
foadolfat <olfatfoad@gmail.com>
2021-01-16 17:53:46 -0800

    Fixed scene sizes

--- commonwealth/src/sample/Project.java
+++ commonwealth/src/sample/Project.java
@@ -9,13 +9,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Home {
-
-    public void goCreate(ActionEvent event) throws IOException {
-        Parent homeRoot = FXMLLoader.load(getClass().getResource("CommonWealth.fxml"));
-        Scene homeScene = new Scene(homeRoot,440,484);
+public class Project {
+    public void goHome(ActionEvent event) throws IOException {
+        Parent homeRoot = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        Scene homeScene = new Scene(homeRoot);
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-        window.setTitle("Create Project");
+        window.setTitle("Home");
         window.setScene(homeScene);
         window.show();
     }
